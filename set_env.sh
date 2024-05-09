94ea10553554ea567ff5213a0b3e2b7bcb5a40fd
xingkungao <1005035266@qq.com>
2024-05-09 10:10:53 +0000

    fix: fix log

---
+++ set_env.sh
@@ -0,0 +1,5 @@
+export CUDA_ROOT_DIR=/usr/local/cuda-12.1/
+export CUDNN_ROOT_DIR=/usr/include/
+export TRT_ROOT_DIR=/usr/lib/x86_64-linux-gnu/
+export LITE_INSTALL_DIR=/path/to/megenginelite-lib
+export LD_LIBRARY_PATH=$LITE_INSTALL_DIR/lib/x86_64/:$LD_LIBRARY_PATH
